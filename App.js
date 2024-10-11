69ca0b8d34a0e7c439169ad82982abd86db1a27e
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2024-10-11 14:35:10 -0600

    CONNECTS TO IOS

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import * as Reanimated from 'react-native-reanimated';
+
+// Initialize Reanimated
+Reanimated.initialize();
+
+
+export default function App() {
+    return (
+        <View>
+            {/* <Text>Hello, Expo!</Text> */}
+        </View>
+    );
+}
